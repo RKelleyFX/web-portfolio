1011e74f6e37807e67e84a53d76ee2b6037fd934
Ryan Kelley <rkelleyfx@gmail.com>
2021-03-04 00:32:16 -0800

    Build GraphQL API and DB

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,126 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateContact = /* GraphQL */ `
+  subscription OnCreateContact {
+    onCreateContact {
+      id
+      firstName
+      lastName
+      email
+      phone
+      message
+      contactReason
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateContact = /* GraphQL */ `
+  subscription OnUpdateContact {
+    onUpdateContact {
+      id
+      firstName
+      lastName
+      email
+      phone
+      message
+      contactReason
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteContact = /* GraphQL */ `
+  subscription OnDeleteContact {
+    onDeleteContact {
+      id
+      firstName
+      lastName
+      email
+      phone
+      message
+      contactReason
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreatePageIntro = /* GraphQL */ `
+  subscription OnCreatePageIntro {
+    onCreatePageIntro {
+      id
+      page
+      body
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdatePageIntro = /* GraphQL */ `
+  subscription OnUpdatePageIntro {
+    onUpdatePageIntro {
+      id
+      page
+      body
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeletePageIntro = /* GraphQL */ `
+  subscription OnDeletePageIntro {
+    onDeletePageIntro {
+      id
+      page
+      body
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreatePortfolioPost = /* GraphQL */ `
+  subscription OnCreatePortfolioPost {
+    onCreatePortfolioPost {
+      id
+      page
+      title
+      intro
+      body
+      featured
+      keywords
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdatePortfolioPost = /* GraphQL */ `
+  subscription OnUpdatePortfolioPost {
+    onUpdatePortfolioPost {
+      id
+      page
+      title
+      intro
+      body
+      featured
+      keywords
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeletePortfolioPost = /* GraphQL */ `
+  subscription OnDeletePortfolioPost {
+    onDeletePortfolioPost {
+      id
+      page
+      title
+      intro
+      body
+      featured
+      keywords
+      createdAt
+      updatedAt
+    }
+  }
+`;
